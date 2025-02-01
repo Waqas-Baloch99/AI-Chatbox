aab29cf60bcae4fd4ede4d0214c5d934cc096e07
Muhammad Waqas Atta <waqaskhosa99@gmail.com>
2025-02-02 00:26:08 +0300

    Update chatbox.py

--- chatbox.py
+++ chatbox.py
@@ -39,7 +39,7 @@ def inject_custom_css():
         
         .user-message {{
     background: rgba(78, 204, 163, 0.1);
-    display: inline-flex;
+    display: flex;
     padding: 1rem;
     border-radius: 15px 15px 0 15px;
     margin: 0.5rem 0 0.5rem auto;
